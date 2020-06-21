c92f058b42fea8c08ac7e3a05e240825be7cfd3e
igSipheR <57493367+igSipheR@users.noreply.github.com>
2020-06-21 20:53:31 +0530

    Update StonePaperScissor.py

--- StonePaperScissor.py
+++ StonePaperScissor.py
@@ -1,6 +1,6 @@
 import sys
-user1 = input("What's your name: ")
-user2 = input("and your name: ")
+user1 = input("1st player name: ")
+user2 = input("2nd player name: ")
 
 user1_answer = input("%s, do you want to choose rock, paper or scissors: " % user1)
 user2_answer = input("%s, do you want to choose rock, paper or scissors: " % user2)
@@ -14,7 +14,7 @@ def compare(u1, u2):
     else:
         return(user2,"wins!")
 
-    elif u1 == 'scissors':
+    if u1 == 'scissors':
         if u2 == 'paper':
             return(user1,"wins!")
         else:
